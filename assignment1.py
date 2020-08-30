eda3c657527c2649695c39d6f2cd6872a2cb11c9
Plath <cooper.plath@my.jcu.edu.au>
2020-08-30 14:15:41 +1000

    Starting off

--- assignment1.py
+++ assignment1.py
@@ -1,14 +1,31 @@
 """
 Replace the contents of this module docstring with your own details
-Name:
-Date started:
+Name: Cooper Plath
+Date started: 30-8-20
 GitHub URL:
 """
 
 
 def main():
-    print("Travel Tracker 1.0 - by <Your Name>")
+    print("Travel Tracker 1.0 - by Cooper Plath")
+    Menu_Input = Menu()
+    print(Menu())
 
 
-if __name__ == '__main__':
-    main()
+def Menu():
+    print("""L - List Places
+    A - Add new place
+    M - Mark a place as visited
+    Q - Quit""")
+    """
+    
+    """
+    Menu_Input = str(input())
+    while Menu_Input != "Q":
+        return Menu_Input
+
+
+
+
+
+main()
